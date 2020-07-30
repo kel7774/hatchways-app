e1a0bc5d4e33a95e59f4d68915c233dd424c176f
Kelli Landry <landry.kel@gmail.com>
2020-07-30 17:11:31 -0500

    add axios, fetch from api, display on main page for part 1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Contact from "./components/Contact";
+import "./App.css";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [students, setStudents] = useState([]);
+
+  const getStudents = async () => {
+    setLoading(true);
+    try {
+      const url = `https://www.hatchways.io/api/assessment/students`;
+      const response = await axios.get(url);
+      setStudents(response.data.students);
+      console.log(response.data.students);
+      setLoading(false);
+    } catch (err) {
+      console.log("Error: ", err);
+    }
+  };
+
+  useEffect(() => {
+    getStudents();
+  }, []);
+
+  if (loading) return "Loading ...";
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header">Hatchways App</header>
+      <main>
+        <Contact students={students} />
+      </main>
     </div>
   );
 }
