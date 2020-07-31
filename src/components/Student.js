578cf124f67c14ac1c95c10bd6139f6896767cd6
Kelli Landry <landry.kel@gmail.com>
2020-07-30 20:26:18 -0500

    Collapse w/ grades displayed

--- src/components/Student.js
+++ src/components/Student.js
@@ -1,8 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
+import { Collapse } from "react-collapse";
 import findAverage from "../helpers/findAverage";
 import Styles from "../styles/StudentStyles";
 
 const Student = ({ students }) => {
+  const [isOpened, setIsOpened] = useState(false);
+  const collapse = () => {
+    setIsOpened(!isOpened);
+  };
   return (
     <Styles>
       <div className="student-container">
@@ -15,8 +20,18 @@ const Student = ({ students }) => {
           <p>Company: {students.company}</p>
           <p>Skill: {students.skill}</p>
           <p>Average: {findAverage(students.grades)}</p>
+          <Collapse isOpened={isOpened}>
+            {students.grades.map((g, index) => (
+              <ul key={index} className="grades-list">
+                <li>
+                  Test {index + 1}: {g}%
+                </li>
+              </ul>
+            ))}
+          </Collapse>
         </div>
       </div>
+      <button onClick={collapse}>+</button>
     </Styles>
   );
 };
