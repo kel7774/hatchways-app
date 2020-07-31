5af8ad1329085d8b8d6c2a8095a014c5f0cdce19
Kelli Landry <landry.kel@gmail.com>
2020-07-30 21:48:38 -0500

    Add tags from react-tags

--- src/components/Student.js
+++ src/components/Student.js
@@ -1,12 +1,31 @@
 import React, { useState } from "react";
+import { WithContext as ReactTags } from "react-tag-input";
 import { Collapse } from "react-collapse";
 import findAverage from "../helpers/findAverage";
 import Styles from "../styles/StudentStyles";
 
+const KeyCodes = {
+  comma: 188,
+  enter: 13,
+};
+
+const delimiters = [KeyCodes.comma, KeyCodes.enter];
+
 const Student = ({ students }) => {
   const [isOpened, setIsOpened] = useState(false);
+  const [tags, setTags] = useState(["react"]);
+
   const collapse = () => {
     setIsOpened(!isOpened);
+  };
+
+  const handleDelete = (i) => {
+    const deleted = tags.filter((tag, index) => index !== i);
+    setTags(deleted);
+  };
+
+  const handleAddition = (tag) => {
+    setTags(tags.push(tag));
   };
   return (
     <Styles>
@@ -22,12 +41,21 @@ const Student = ({ students }) => {
           <p>Average: {findAverage(students.grades)}</p>
           <Collapse isOpened={isOpened}>
             {students.grades.map((g, index) => (
-              <ul key={index} className="grades-list">
-                <li>
-                  Test {index + 1}: {g}%
-                </li>
-              </ul>
+              <div>
+                <ul key={index} className="grades-list">
+                  <li>
+                    Test {index + 1}: {g}%
+                  </li>
+                </ul>
+              </div>
             ))}
+            <ReactTags
+              tags={tags}
+              handleDelete={handleDelete}
+              handleAddition={handleAddition}
+              draggable={false}
+              delimiters={delimiters}
+            />
           </Collapse>
         </div>
       </div>
